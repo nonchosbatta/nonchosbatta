d68c3e396d60d2f2ded59f394565b4b21740f8c4
RoxasShadow <webmaster@giovannicapuano.net>
2015-08-09 14:59:35 +0200

    Use letter_opener in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.action_mailer.delivery_method = :letter_opener
+
   config.cache_store = :dalli_store, 'localhost', { namespace: 'Nonchosbatta_v1', pool_size: 5, compress: true }
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
