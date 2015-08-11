81347698f65a24f5d877b3d73594d43e9cf04fc3
RoxasShadow <webmaster@giovannicapuano.net>
2015-08-11 19:43:35 +0200

    Fill factories of fansubs, shows and users

--- spec/factories/shows.rb
+++ spec/factories/shows.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :show do
-    
+    title 'Japan Anime Expo'
+    short_title 'JAE'
+    episodes_count 50
+    status { Show.statuses[:doing] }
+    format { Show.formats[:ona] }
+    started_at { Date.parse('2015/04/01') }
   end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    first_name 'Kimi'
+    last_name 'Aoi'
+    sequence(:email) { |n| "kimi#{n}@mozestudio.com" }
+    password 'uzume1648'
+    password_confirmation 'uzume1648'
+    confirmed_at 1.hour.ago
   end
-
 end
